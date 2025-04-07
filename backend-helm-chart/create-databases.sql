88bbdd0d6b1fecd16a30098dc3ab57cbadacd2c5
prashanth7993 <jprashanth429@gmail.com>
2025-04-07 07:14:22 +0000

    Updated filed

---
+++ backend-helm-chart/create-databases.sql
@@ -0,0 +1,20 @@
+
+CREATE DATABASE "vendor-service";
+
+CREATE DATABASE "authentication-service";
+
+CREATE DATABASE "vehicles-service";
+
+CREATE DATABASE "user-service";
+
+CREATE DATABASE "bookings-service";
+
+CREATE DATABASE "customer-support";
+
+CREATE DATABASE "review-service";
+
+CREATE DATABASE "coupon-service";
+
+CREATE DATABASE "payment-service";
+
+CREATE DATABASE "documents-service";
